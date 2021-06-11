13d807e3ecf03c9f811cd60af21a176ebb5cc169
Emmanuel Norambuena <38821970+eanorambuena@users.noreply.github.com>
2021-06-11 16:08:27 +0000

    Test v2.0.3 Bugs PASSED.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from pfcf.read import read,executepfcf
-from pfcf.utils import execute
-f1="example"
+from pyforchange.pfcf.read import executepfcf
+from pyforchange.pfcf.utils import execute
+f1="examples/example"
 f2="ex_qiskit"
 f3="ex_python"
 f4="ex_wolfram"
-#executepfcf(f4)
-execute("python_compile")
+executepfcf(f1)
+#execute("python_compile")
--- wolfram_compile.py
+++ wolfram_compile.py
@@ -2,4 +2,3 @@ from wolframclient.evaluation import WolframLanguageSession
 from wolframclient.language import wl, wlexpr
 session = WolframLanguageSession()
 session.evaluate(wlexpr('Range[5]'))
-session.terminate()
