3db270dd01baa3c989c1800f0c25b6c077bfbe65
Emmanuel Norambuena <eanorambuena@uc.cl>
2021-06-23 20:46:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from pyforchange.pfcf.read import executepfcf
-from pyforchange.pfcf.utils import execute
-f1="example"
-f2="ex_qiskit"
-f3="ex_python"
-f4="ex_wolfram"
-executepfcf("examples/"+f1)
-#execute("python_compile")
+from nqs.read import execute
+f1="circuit"
+execute("examples/"+f1)
+
